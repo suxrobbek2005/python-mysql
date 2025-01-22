f992709b8a938dc67c48c739a5f141f937c0635a
djumanov <djumanovdev@gmail.com>
2025-01-22 10:49:51 +0500

    task

--- main.py
+++ main.py
@@ -5,8 +5,36 @@ connection = mysql.connector.connect(
     host=settings.host,
     user=settings.user,
     password=settings.password,
-    port=settings.port
+    port=settings.port,
+    # database=settings.db_name
 )
 
-if connection.is_connected():
-    print("mysql ga muvaffaqiyatli ulandik.")
+cursor = connection.cursor()
+
+cursor.execute("USE shop")
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS hodimlar (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(64),
+    department VARCHAR(128),
+    locaiton TEXT
+)
+''')
+
+# cursor.execute("SHOW TABLES")
+
+rows = cursor.fetchall()
+
+print(rows)
+
+cursor.close()
+connection.close()
+
+
+# educaiton db
+# students table (id, name, grade, age)
+# 10 students
+# select all
+# filter age increasing
+
